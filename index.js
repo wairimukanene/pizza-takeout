a10c789689dfcb6b6cc7fcf05f8799cec4f5b6b8
wairimu.Kanene <mercykanene254@gmail.com>
2022-03-24 08:14:52 +0300

    commit-modified html,js and css

--- index.js
+++ index.js
@@ -1,5 +1,3 @@
-let price , crust_price, topping_price ;
-let total = 0;
 function Getpizza( name,size,crust,topping, total ){
   this.name = name;
   this.size = size;
@@ -16,7 +14,7 @@ $(document).ready(function(){
   }
   $("#proceed").click(function(){
     $(".table").show();
-    $(".btn-checkout").show();
+    $(".checkout").show();
     let pName = $("#name option:selected").text();
     let pSize = $("#size option:selected").val();
     let pTopping = $("#toppings option:selected").val();
@@ -35,7 +33,7 @@ $(document).ready(function(){
     $("#pizzatopping").html(pizzaTopping);
     $("#totals").html(total);
 
-    console.log(totals);
+    console.log(checkoutTotal);
     $("#proceed").click(function() {
       let pName=$("#name option:selected").text();
       let pSize = $("#size option:selected").val();
@@ -64,5 +62,43 @@ $(document).ready(function(){
       console.log("Your total bills is sh. "+checkoutTotal);
       $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
     });
+    $("button.deliver").click(function(){
+      $(".pizzatable").hide();
+      $(".choice h2").hide();
+      $(".delivery").slideDown(1000);
+      $("#addedprice").hide();
+      $("button.deliver").hide();
+      $("#pizzatotal").hide();
+      let deliveryamount= checkoutTotal+150;
+      console.log("You will pay sh. "+deliveryamount+" on delivery");
+      $("#totalbill").append("Your bill plus delivery fee is: "+deliveryamount);
+    });
+    $("button#final-order").click(function(event){
+      event.preventDefault();
+
+      $("#pizzatotal").hide();
+      $(".delivery").hide();
+      $("button#final-order").hide();
+      let deliveryamount= checkoutTotal+150;
+      console.log("Final Bill is: "+deliceryamount);
+      let person = $("input#name").val();
+      let phone = $("input#phone").val();
+      let location = $("input#location").val();
+
+      if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
+  
+        $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliveryamount);
+        $("#totalbill").hide();
+        $("#finallmessage").slideDown(1200);
+      }
+      else {
+        alert("Please fill in the details for delivery!");
+        $(".delivery").show();
+        $("button#final-order").show();
+      }
+    });
+   event.preventDefault();
+  });
+});
 
       
