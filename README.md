8c485a661611dab729cb6c43f6221149795fc38c
wairimu.Kanene <mercykanene254@gmail.com>
2022-03-25 00:06:01 +0300

    added readme,modified js

---
+++ README.md
@@ -0,0 +1,70 @@
+**PIZZA-TAKEOUT**
+
+### It is a description of my pizza-Takeout
+
+---
+
+## Table of Content
+
++ [WebApplication](it a webapllication that helps the client to like order pizza and pay through the web application thus saving time.)
++ [installation-Requirement](One needs a phone,laptop or a desktop)
++ [Technology-used](The technology used in my webApplication pizzatakeout its html-for structure,css-for styling bootstrap and javascript-for functionality) 
++ [Reference](W3schools,Markdown,Youtube)
++ [Author-information](Wairimu Kanene,mercykanene254@gmail.com)
++ [Designer-information](Wairimu kanene)
+
+---
+#### Description
+
+This web application is a pizza web application that enables user to like select the type of pizza,flavor,crust and size then order and make payments.
+---
+### Installation process
+Make sure you have a phone, Desktop or a laptop.
+Internet Connectivity.
+Battery full.
+Copy the website link on the github,
+git clone on the terminal
+Then paste and enter.
+
+---
+
+### live link
+[livelink](https://wairimukanene.github.io/pizza-takeout/)
+
+
+---
+
+---
+## Technology Used
+HTML- i used html to design the structure.
+CSS-i used css for styling
+Javascript-used for functionality
+
+
+### ** Reference Used ** ###
+W3schools;
+Markdown;
+Youtube;
+
+---
+## Licence
+
+MIT Licence
+MIT Copyright (c) [2022][WairimuKanene]
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+
+---
+### Authors Information
+[WairimuKanene](https://mail.google.com/mail/u/0/#inbox)
+[Slack](Mercy Kanene)
+
+
+---
+## Design Information
+WairimuKanene
--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function Getpizza( name,size,crust,topping, total ){
   this.name = name;
   this.size = size;
-  this.toppings = toppings;
-  this.crust = crust;
+  this.crust =crust;
+  this.topping = topping;
   this.total = total;
 }
 $(document).ready(function(){
@@ -13,91 +13,57 @@ $(document).ready(function(){
     pizzaCrust = $("#crust option:selected").text("Select Crust");
   }
   $("#proceed").click(function(){
-    $(".table").show();
-    $(".checkout").show();
+    $(".choice").show();
+   
     let pName = $("#name option:selected").text();
-    let pSize = $("#size option:selected").val();
-    let pTopping = $("#toppings option:selected").val();
-    let pCrust = $("#crust option:selected").val();
-
-    let pName = parseInt($("#name option:selected").text());
-    let pSize = parseInt($("#size option:selected").val());
-    let pTopping = parseInt($("#toppings option:selected").val());
-    let pCrust = parseInt($("#crust option:selected").val());
-    let total = pName + pSize + pTopping + pCrust;
+    let pSize =$("#size option:selected") .text();
+    let pCrust =$("#crust option:selected") .text();
+    let pTopping=$("#topping  option:selected") .text();
+    
+    let pSizePrice = parseInt($("#size option:selected").val());
+    let pToppingPrice = parseInt($("#topping option:selected").val());
+    let pCrustPrice = parseInt($("#crust option:selected").val());
+    let total = pSizePrice +  pCrustPrice +  pToppingPrice;
     let checkoutTotal = total;
-
-    $("#pizzaname").html(pizzaName);
-    $("#pizzasize").html(pizzaToppings);
-    $("#pizzacrust").html(pizzaCrust);
-    $("#pizzatopping").html(pizzaTopping);
-    $("#totals").html(total);
-
     console.log(checkoutTotal);
-    $("#proceed").click(function() {
-      let pName=$("#name option:selected").text();
-      let pSize = $("#size option:selected").val();
-      let pTopping = $("#toppings option:selected").val();
-      let pCrust = $("#crust option:selected").val();
 
-      let pName = parseInt($("#name option:selected").text());
-      let pSize = parseInt($("#size option:selected").val());
-      let pTopping = parseInt($("#toppings option:selected").val());
-      let pCrust = parseInt($("#crust option:selected").val());
-      let total = pName + pSize + pTopping + pCrust;
-      let checkoutTotal = total;
-      checkoutTotal = checkoutTotal + total;
+    $("#pizzaname").html(pName);
+    $("#pizzasize").html(pSize);
+    $("#pizzacrust").html(pCrust);
+    $("#pizzatopping").html(pTopping);
+    $("#totals").html(total);
+    $("#cost").html("sh"+ checkoutTotal);
+    
+
+    $("#addPizza").click(function() {
+      let pName = $("#name option:selected").text();
+      let pSize =$("#size option:selected") .text();
+      let pCrust =$("#crust option:selected") .text();
+      let pTopping=$("#topping  option:selected") .text();
       
-      let newOrder = new Order( pizzaName ,pizzaSize, pizzaTopping, pizzaCrust, total);
-      let newOrderSummary = '<tr>' + '<td id="pizzaname">' + newOrder.name + '</td><td id="pizzasize">' + newOrder.size + '</td><td id="pizzacrust">' + newOrder.crust + '</td><td id="pizzatopping">' +newOrder.topping + '</td><td id="totals">' + newOrder.totals + '</td></tr>'
-      $("#totals").append(newOrderSummary);  
+      let pSizePrice = parseInt($("#size option:selected").val());
+      let pToppingPrice = parseInt($("#topping option:selected").val());
+      let pCrustPrice = parseInt($("#crust option:selected").val());
+      let total = pSizePrice +  pCrustPrice +  pToppingPrice;
+    
+       checkoutTotal = checkoutTotal + total;
+      
+      let newPizza = new Getpizza( pName ,pSize, pCrust, pTopping, total);
+      let newPizzaSummary = '<tr>' + '<td id="pizzaname">' + newPizza.name + '</td><td id="pizzasize">' + newPizza.size + '</td><td id="pizzacrust">' + newPizza.crust + '</td><td id="pizzatopping">' +newPizza.topping + '</td><td id="totals">' + newPizza.total + '</td></tr>'
+      $("#ordersmade").append(newPizzaSummary);  
       console.log(checkoutTotal);
+      $("#cost").html("sh"+ checkoutTotal);
     });
-    // Checkout button
-    $("button#checkout").click(function(){ 
-      $("button#checkout").hide();
-      $("button.addPizza").hide();
-      $("button.deliver").slideDown(1000);
-      $("#addedprice").slideDown(1000);
-      console.log("Your total bills is sh. "+checkoutTotal);
-      $("#pizzatotal").append("Your bill is sh. "+checkoutTotal);
+    $("#checkout").click(function() {
+      $(".location").show();
+      $(".homeDelivery").show();
     });
-    $("button.deliver").click(function(){
-      $(".pizzatable").hide();
-      $(".choice h2").hide();
-      $(".delivery").slideDown(1000);
-      $("#addedprice").hide();
-      $("button.deliver").hide();
-      $("#pizzatotal").hide();
-      let deliveryamount= checkoutTotal+150;
-      console.log("You will pay sh. "+deliveryamount+" on delivery");
-      $("#totalbill").append("Your bill plus delivery fee is: "+deliveryamount);
-    });
-    $("button#final-order").click(function(event){
-      event.preventDefault();
-
-      $("#pizzatotal").hide();
-      $(".delivery").hide();
-      $("button#final-order").hide();
-      let deliveryamount= checkoutTotal+150;
-      console.log("Final Bill is: "+deliceryamount);
-      let person = $("input#name").val();
-      let phone = $("input#phone").val();
-      let location = $("input#location").val();
-
-      if ($("input#name").val() && $("input#phone").val() && $("input#location").val()!=""){
-  
-        $("#finallmessage").append(person+", We have recieved your order and it will be delivered to you at "+location+ ". Prepare sh. "+deliveryamount);
-        $("#totalbill").hide();
-        $("#finallmessage").slideDown(1200);
-      }
-      else {
-        alert("Please fill in the details for delivery!");
-        $(".delivery").show();
-        $("button#final-order").show();
-      }
-    });
-   event.preventDefault();
+      $("#deliver").click(function() {
+        checkoutTotal=checkoutTotal+ 150;
+        $("#cost").html("sh"+ checkoutTotal);
+        let location=$("#place").val();
+        $(".home").html(location);
+      });
   });
 });
 
